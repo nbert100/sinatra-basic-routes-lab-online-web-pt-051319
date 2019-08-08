21211ed8211aff8245e2c06e7d1fc8bfbe46054a
Nia Bert <nbert14@gmail.com>
2019-08-08 22:37:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,9 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-get '/' do
-  "Hello, World!"
+get '/name' do
+  "My name is __"
 end
 
+get '/hometown' do
+  "My hometown is __"
+end
+
+get '/favorite-song' do
+  "My favorite song is __"
+end
 
 end
